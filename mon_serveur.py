db0d33e52f1c7987353ae569765a76df197f7794
Bernoussi Tarek <80513120+Mildor@users.noreply.github.com>
2022-10-21 17:35:02 +0200

    Update mon_serveur.py

--- mon_serveur.py
+++ mon_serveur.py
@@ -1,26 +1,30 @@
 # coding: utf-8 
 
-import multiprocessing
-from os import kill
 import socket as socke
 from socket import *
 import threading
 
-
+#List d'utilisateurs connecté
 users = []
 
+#objet client qui correspond a un client qui se connecte au serveur, il hérite de la class threading afin de pouvoir activé plusieurs connexion en même temps
 class ClientThread(threading.Thread):
-
+    
+    #initialisation de la class ClientThread
     def __init__(self, ip, port, clientsocket):
         threading.Thread.__init__(self)
         self.ip = ip
         self.port = port
         self.clientsocket = clientsocket
         print("[+] Nouveau thread pour %s %s" % (self.ip, self.port, ))
-
+    
+    #Fonction run qui s'active a la creation d'un clientThread 
     def run(self): 
         print("Connexion de %s %s" % (self.ip, self.port, ))
-        while True:
+        
+        #Boucle qui permet la réception de message
+        i = 1
+        while i==1:
             data = self.clientsocket.recv(2048)
             data = data.decode()
             client = socke.gethostbyaddr(self.ip)
@@ -28,14 +32,15 @@ class ClientThread(threading.Thread):
             self.multicast(data)
             if data is None or data == "" or data=="quit":
                 print("Connexion de %s %s coupé" % (self.ip, self.port, ))
-                break
-
+                i = 2
+    
+    #Fonction qui envoie un message
     def multicast(self, message):
         for user in users:
             if user != self.clientsocket:
                 user.send(message.encode())
 
-
+#Boucle de création du serveur, puis de création de Thread client 
 while True:
     sock = socket(AF_INET, SOCK_STREAM)
     sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
