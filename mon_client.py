eb8a2507e2febc62d1dcfe9300178e182ac93163
Bernoussi Tarek <80513120+Mildor@users.noreply.github.com>
2022-10-21 17:31:23 +0200

    Update mon_client.py

--- mon_client.py
+++ mon_client.py
@@ -1,20 +1,28 @@
 from socket import *
 from threading import *
 
+#Connexion au PC Serveur
+#target correspondant au nom/ip du serveur serveur
+#port corresspondant au port du serveur
+target = "info24-15"
+port = "9999"
 connexion = socket(AF_INET, SOCK_STREAM)
 connexion.connect(("info24-15", 9999))
 
-def thread_en_envoie():
+#Fonction d'envoie de message
+def envoie():
     while True:
         message = input()
         if message:
             message = gethostname()+" : "+message
             connexion.send(message.encode())
 
+#Fonction réception message
 def thread_en_reception():
     while True:
         message = connexion.recv(1024).decode()
         print(message)
 
+#création de thread pour la récéption et l'envoie de message
 Thread(target=thread_en_envoie).start()
 Thread(target=thread_en_reception).start()
